6e62b3d38c2e30bf6fb72403272974f955f258bf
Leonard Imbert <imbert418@gmail.com>
2021-02-04 10:12:28 +0100

    Fix Geometry

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -51,7 +51,7 @@ G4VPhysicalVolume * DetectorConstruction::Construct() {
   G4Element * N = new G4Element("Nitrogen", "N", z=7., a= 14.01*g/mole);
   G4Element * O = new G4Element("Oxygen"  , "O", z=8., a= 16.00*g/mole);
 
-  G4Material * Air = new G4Material("Air", density= /*1.29*/0*mg/cm3, nel=2);
+  G4Material * Air = new G4Material("Air", density= 1.29, nel=2);
   Air->AddElement(N, 70*perCent);
   Air->AddElement(O, 30*perCent);
 
@@ -59,7 +59,7 @@ G4VPhysicalVolume * DetectorConstruction::Construct() {
   G4Element * C = new G4Element("Carbon", "C", z=6., a= 12.00*g/mole);
   G4Element * H = new G4Element("Hydrogen", "H", z =1., a= 1.0079*g/mole);
 
-  G4Material * Detect = new G4Material("Detector", density= 0.8*g/cm3, nel=2);
+  G4Material * Detect = new G4Material("Detector", density= 1.03*g/cm3, nel=2);
   Detect->AddElement(C, 47.37*perCent);
   Detect->AddElement(H, 52.63*perCent);
 
@@ -87,10 +87,10 @@ G4VPhysicalVolume * DetectorConstruction::Construct() {
 
   G4Box * rs_box   = new G4Box("rs", rs_x, rs_y, rs_z);
   rs1_log           = new G4LogicalVolume(rs_box, Detect, "rs1_log");
-  r1s1_phys        = new G4PVPlacement(0, G4ThreeVector(0, rs_y, -rs_z - r2_z), rs1_log, "r1s1", experimentalHall_log, false, 0);
+  r1s1_phys        = new G4PVPlacement(0, G4ThreeVector(0, rs_y+0.01, -rs_z - r2_z - 0.01), rs1_log, "r1s1", experimentalHall_log, false, 0);
 
   // =========== Square 2
-  r1s2_phys        = new G4PVPlacement(0, G4ThreeVector(0, -rs_y, -rs_z - r2_z), rs1_log, "r1s2", experimentalHall_log, false, 1);
+  r1s2_phys        = new G4PVPlacement(0, G4ThreeVector(0, -rs_y-0.01, -rs_z - r2_z - 0.01), rs1_log, "r1s2", experimentalHall_log, false, 1);
 
   // ======== Raquette 2
   G4Box * r2_box   = new G4Box("r2", r2_x, r2_y, r2_z);
@@ -100,9 +100,9 @@ G4VPhysicalVolume * DetectorConstruction::Construct() {
   // ======== Raquette 3
   rs3_log           = new G4LogicalVolume(rs_box, Detect, "rs3_log");
   // =========== Square 1
-  r3s1_phys        = new G4PVPlacement(0, G4ThreeVector(0, rs_y, +rs_z + r2_z), rs3_log, "r3s1", experimentalHall_log, false, 2);
+  r3s1_phys        = new G4PVPlacement(0, G4ThreeVector(0, rs_y+0.01, +rs_z + r2_z + 0.01), rs3_log, "r3s1", experimentalHall_log, false, 2);
   // =========== Square 2
-  r3s2_phys        = new G4PVPlacement(0, G4ThreeVector(0, -rs_y, +rs_z + r2_z), rs3_log, "r3s2", experimentalHall_log, false, 3);
+  r3s2_phys        = new G4PVPlacement(0, G4ThreeVector(0, -rs_y-0.01, +rs_z + r2_z + 0.01), rs3_log, "r3s2", experimentalHall_log, false, 3);
 
   // ====== Vis attributes ======
   
